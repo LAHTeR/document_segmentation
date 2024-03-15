5bf5296c72f9dc7081aad6cc219c6a93bc2d9761
Carsten Schnober <c.schnober@esciencecenter.nl>
2024-03-15 15:34:17 +0100

    Add PageTest.test_from_pagexml() test case.

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,4 +36,4 @@ def mock_request(inventory_nr: int = TEST_INV_NR):
 CWD = Path(__file__).parent.absolute()
 DATA_DIR = CWD / "data"
 GENERALE_MISSIVEN_CSV = DATA_DIR / "Overzicht van Generale Missiven in 1.04.02 v.3.csv"
-TEST_FILE = (DATA_DIR / str(inventory)).with_suffix(".zip")
+TEST_FILE = (DATA_DIR / str(TEST_INV_NR)).with_suffix(".zip")
