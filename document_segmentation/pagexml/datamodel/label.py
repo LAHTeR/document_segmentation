8f6b419a63450ba9b16ef617173b587e9dcbcde4
Carsten Schnober <c.schnober@esciencecenter.nl>
2024-03-25 15:55:38 +0100

    Remove dataset.py.

--- document_segmentation/pagexml/datamodel/label.py
+++ document_segmentation/pagexml/datamodel/label.py
@@ -1,5 +1,4 @@
 from enum import IntEnum
-from typing import Iterable
 
 
 class Label(IntEnum):
@@ -18,32 +17,3 @@ class Label(IntEnum):
             list[int]: A list of integers representing the label.
         """
         return [int(self == label) for label in Label]
-
-    def successor(self) -> "Label":
-        """Return the successor of the label.
-
-        Returns:
-            Label: The successor of the label.
-        """
-        if self == Label.BEGIN:
-            return Label.IN
-        elif self == Label.IN:
-            return Label.IN
-        elif self == Label.END or self == Label.OUT:
-            return Label.OUT
-        return Label(self.value + 1)
-
-    @staticmethod
-    def map_scores(scores: Iterable[float]) -> dict[str, float]:
-        """Map a list of scores to a dictionary of label names and scores.
-
-        Args:
-            scores (Iterable[float]): List of scores with the same length as Label.
-        Returns:
-            dict[str, float]: Dictionary mapping label names to scores.
-        """
-        if len(scores) != len(Label):
-            raise ValueError(
-                f"Expected {len(Label)} scores, got {len(scores)}: {scores}"
-            )
-        return {label.name: score for label, score in zip(Label, scores)}
