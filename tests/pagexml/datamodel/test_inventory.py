b748fe8ea91fc754dcc569189a84d48443502dc3
Carsten Schnober <c.schnober@esciencecenter.nl>
2024-03-25 17:02:45 +0100

    Add TestInventory methods.

--- tests/pagexml/datamodel/test_inventory.py
+++ tests/pagexml/datamodel/test_inventory.py
@@ -1,14 +1,65 @@
+import pytest
+
 from document_segmentation.pagexml.datamodel.inventory import Inventory
+from document_segmentation.pagexml.datamodel.label import Label
+from document_segmentation.pagexml.datamodel.page import Page
 
 
 class TestInventory:
+    def test_annotate_scan(self, tmp_path):
+        inventory = Inventory(
+            inv_nr=1201,
+            inventory_part="A",
+            pages=[
+                Page(
+                    label=Label.UNK,
+                    scan_nr=1,
+                    doc_id="doc1",
+                    external_ref="ref1",
+                    regions=[],
+                ),
+                Page(
+                    label=Label.UNK,
+                    scan_nr=2,
+                    doc_id="doc1",
+                    external_ref="ref1",
+                    regions=[],
+                ),
+                Page(
+                    label=Label.UNK,
+                    scan_nr=3,
+                    doc_id="doc1",
+                    external_ref="ref1",
+                    regions=[],
+                ),
+            ],
+        )
+
+        expected_labels = [Label.UNK, Label.BEGIN, Label.UNK]
+        inventory.annotate_scan(2, Label.BEGIN)
+
+        for page, expected_label in zip(inventory.pages, expected_labels):
+            assert page.label == expected_label
+
+        with pytest.raises(ValueError):
+            inventory.annotate_scan(0, Label.END)
+            inventory.annotate_scan(4, Label.END)
+
     def test_download(self, mock_request, tmp_path):
-        expected_pages = 806
+        inventory = Inventory.download(
+            inv_nr=1201, inventory_part="", target_directory=tmp_path
+        )
+        assert inventory.inv_nr == 1201
+        assert inventory.inventory_part == ""
+        assert len(inventory.pages) == 806
 
-        inventory = Inventory.download(inv_nr=1201, target_directory=tmp_path)
+        assert (
+            Inventory.load_or_download(
+                inv_nr=1201, inventory_part="", inventory_dir=tmp_path
+            )
+            == inventory
+        )
 
-        assert len(inventory) == expected_pages
-
-        with open(Inventory.local_file(1201, "", tmp_path), "rt") as f:
-            _json = f.read()
-            assert Inventory.model_validate_json(_json) == inventory
+    def test_localfile(self, tmp_path):
+        assert Inventory.local_file(1201, "A", tmp_path) == tmp_path / "1201_A.json"
+        assert Inventory.local_file(1201, "", tmp_path) == tmp_path / "1201.json"
